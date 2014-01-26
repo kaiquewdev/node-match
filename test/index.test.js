d9675095610deb8a2e33bf71a13d1576ee020b1b
Kaique da Silva <kaique.developer@gmail.com>
2014-01-26 17:44:53 -0200

    Logical operations was added
    "and" and "or" operations working in the flux

--- test/index.test.js
+++ test/index.test.js
@@ -118,4 +118,122 @@ describe('Match', function () {
         .end( completeMatchingHandler );
     });
   });
+
+  describe('logical operation', function () {
+    it('and with string operation', function ( done ) {
+      var actual = 'fizzbuzzbuuz';
+      var token = 'buzz';
+      var secondToken = 'buuz';
+      var expected = [ actual, 'boo', 'boo' ].join('');
+
+      function completeMatchingHandler () {
+        var self = this; 
+
+        self
+          .input
+          .should
+          .eql( expected );
+
+        done();
+      }
+
+      function tokenHandler () {
+        var self = this;
+
+        self.input += 'boo';
+      }
+
+      match( actual )
+        .when( token, tokenHandler )
+        .and( secondToken, tokenHandler )
+        .end( completeMatchingHandler );
+    });
+
+    it('and with regexp operation', function ( done ) {
+        var actual = 'fizzbuzzbuuz';
+        var token = /buzz/;
+        var secondToken = /buuz/;
+        var expected = [ actual, 'boo', 'boo' ].join('');
+
+        function completeMatchingHandler () {
+          var self = this; 
+
+          self
+            .input
+            .should
+            .eql( expected );
+
+          done();
+        }
+
+        function tokenHandler () {
+          var self = this;
+
+          self.input += 'boo';
+        }
+
+        match( actual )
+          .when( token, tokenHandler )
+          .and( secondToken, tokenHandler )
+          .end( completeMatchingHandler );
+    });
+
+    it('or with string operation', function ( done ) {
+      var actual = 'fizzbuzzbuuz';
+      var token = 'buzz';
+      var secondToken = 'bust';
+      var expected = [ actual, 'boo', 'boo' ].join('');
+
+      function completeMatchingHandler () {
+        var self = this; 
+
+        self
+          .input
+          .should
+          .eql( expected );
+
+        done();
+      }
+
+      function tokenHandler () {
+        var self = this;
+
+        self.input += 'boo';
+      }
+
+      match( actual )
+        .when( token, tokenHandler )
+        .or( secondToken, tokenHandler )
+        .end( completeMatchingHandler );
+    });
+
+    it('or with regexp operation', function ( done ) {
+      var actual = 'fizzbuzzbuuz';
+      var token = /buzz/;
+      var secondToken = /bust/;
+      var expected = [ actual, 'boo', 'boo' ].join('');
+
+      function completeMatchingHandler () {
+        var self = this; 
+
+        self
+          .input
+          .should
+          .eql( expected );
+
+        done();
+      }
+
+      function tokenHandler () {
+        var self = this;
+
+        self.input += 'boo';
+      }
+
+      match( actual )
+        .when( token, tokenHandler )
+        .or( secondToken, tokenHandler )
+        .end( completeMatchingHandler );
+    });
+  });
 });
